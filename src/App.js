29a7dd7946683ba43b402e3669baf3f7b35d070b
grantfogle <grantjfogle@gmail.com>
2018-11-21 13:09:00 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Button } from 'reactstrap';
+
+const Home = () => (
+  <div>
+    <h1>Home</h1>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <p>About</p>
+  </div>
+)
+
+const Code = () => (
+  <div>
+    Code
+  </div>
+)
+
+const Contact = () => (
+  <div>
+    Contact
+  </div>
+)
+
+const info = () => (
+  <div>
+    info
+  </div>
+)
+
+const MainMenu = () => {
+  return (
+    <div>
+      <Link to="/">
+        <button>Home</button>
+      </Link>
+      <Link to="/about">
+        <button>About</button>
+      </Link>
+      <Link to="/code">
+        <button>Code</button>
+      </Link>
+      <Link to="/code">
+        <button>contact</button>
+      </Link>
+      <Link to="/info">
+        <button>Info</button>
+      </Link>
+    </div>
+  )
+}
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
           </a>
-        </header>
-      </div>
+            <MainMenu />
+          </header>
+          <div>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/code" component={Code} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/info" component={info} />
+          </div>
+
+        </div>
+      </Router>
     );
   }
 }
